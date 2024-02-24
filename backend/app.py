1f4a9977b4b211cc2386f5657653375d224b0b2a
meg217 <117707592+meg217@users.noreply.github.com>
2024-02-24 14:04:44 -0600

    database setup

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 # API endpoint to get random question
 @app.route('/', methods=['GET'])
 def get_random_question():
-    # Create a new SQLite connection and cursor for each request
     with sqlite3.connect('8ball.db') as conn:
         cursor = conn.cursor()
         cursor.execute('SELECT question FROM questions ORDER BY RANDOM() LIMIT 1')
