2108c7dbc8258481984131c69004c214a00478d6
JDMxSpartan <kevindehoyos66066@gmail.com>
2024-02-24 16:31:21 -0600

    title

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
 // App.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
   const [question, setQuestion] = useState("");
-  const [response, setResponse] = useState('');
+  const [response, setResponse] = useState("");
 
   useEffect(() => {
     fetchQuestion();
   }, []);
 
   const fetchQuestion = () => {
-    axios.get('https://your-flask-backend-url/api/question')
-      .then(response => {
+    axios
+      .get("https://your-flask-backend-url/api/question")
+      .then((response) => {
         setQuestion(response.data.question);
       })
-      .catch(error => {
-        console.error('Error fetching question:', error);
+      .catch((error) => {
+        console.error("Error fetching question:", error);
       });
   };
 
   const handleMagic8BallClick = () => {
     // need to make onclick and have the magic 8ball shake
-    const responses = ['Yes', 'No', 'Maybe', 'Try again later', 'Definitely'];
-    const randomResponse = responses[Math.floor(Math.random() * responses.length)];
+    const responses = ["Yes", "No", "Maybe", "Try again later", "Definitely"];
+    const randomResponse =
+      responses[Math.floor(Math.random() * responses.length)];
     setResponse(randomResponse);
   };
 
   const handleNextQuestionClick = () => {
     fetchQuestion();
-    setResponse('');
+    setResponse("");
   };
 
   return (
     <div className="App">
+      <img src="/assets/Title.png" type="png" alt="Title Screen"></img>
       {/* Story Video Intro */}
-      <video autoPlay loop muted>
-        <source src="LastQuestionsOfADino.mp4" type="video/mp4" />
-      </video>
 
       {/* Magic 8 Ball */}
       <div className="magic-8-ball" onClick={handleMagic8BallClick}>
