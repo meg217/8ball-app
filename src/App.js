d14c3dc22d0b5f38c54e6b5a1c70d6661fc965ec
JDMxSpartan <kevindehoyos66066@gmail.com>
2024-02-24 13:47:44 -0600

    title replace

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 // App.js
-import React, { useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [question, setQuestion] = useState('');
+  const [question, setQuestion] = useState("");
 
   const getNextQuestion = () => {
-    axios.get('https://your-flask-backend-url/api/question')
-      .then(response => {
+    axios
+      .get("https://your-flask-backend-url/api/question")
+      .then((response) => {
         setQuestion(response.data.question);
       })
-      .catch(error => {
-        console.error('Error fetching question:', error);
+      .catch((error) => {
+        console.error("Error fetching question:", error);
       });
   };
 
   return (
     <div className="App">
-      <video autoPlay loop muted>
-        <source src="/LastQuestionsofaDino.mp4" type="video/mp4" />
-      </video>
+      <img
+        class="Title_Image"
+        src="assets/Title.png"
+        type="img/png"
+        alt="Title Screen"
+      />
       <h1>8 Ball</h1>
       <button onClick={getNextQuestion}>Next Question</button>
       <div className="question">{question}</div>
       {/* Add 8 ball component with shake animation */}
-    </div> 
+    </div>
   );
 }
 
